59a739d242161db6db881151bd8ee9c832f3bb9c
Kévin Rocha <neonix75@gmail.com>
2020-08-09 16:06:56 +0200

    Separation of the masonry js part from the main app.js

--- app.js
+++ app.js
@@ -37,14 +37,6 @@ function navSlide() {
 navSlide();
 
 
-// Masonry Grid
-var elem = document.querySelector('.grid');
-    var msnry = new Masonry( elem, {
-        itemSelector: '.grid-item',
-        gutter: 8
-});
-
-
 
 
 /*
