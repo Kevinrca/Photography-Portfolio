771928fc4ef9995097ab8ff7a3d80181bd289ddd
Kévin Rocha <neonix75@gmail.com>
2020-08-12 18:00:10 +0200

    Swithing from masonry app to masy app for the image grid in the portfolio page

---
+++ app/js/masy.js
@@ -0,0 +1,16 @@
+var macy = Macy({
+    container: '.grid',
+    trueOrder: false,
+    waitForImages: false,
+    margin: {
+        x: 12,
+        y: 5
+    },
+    columns: 4,
+    breakAt: {
+        1300: 3,
+        940: 2,
+        680: 1
+    }
+});
+
